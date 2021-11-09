10e0f9327cf743ca1d9614330f8ae2bed20222cf
GiaBaone <102190252@sv1.dut.udn.vn>
2021-11-09 18:52:55 +0700

    init semarphone 9.11.21 ✨

--- src/diners/Philosopher.java
+++ src/diners/Philosopher.java
@@ -1,7 +1,12 @@
 package diners;
 
 public class Philosopher {
+	public  final static int THINKING=0;
+    public final static int HUNGRY=1;
+    public final static int EATING=2;
 	public static MainFram mf=new MainFram();
+	
+	
 	public static void main(String[] args) {
 		mf.setVisible(true);
 	}
